0b47362321fc74e42859129e90a86fdb8160b027
Tatianaaaaaaa <118683978+Tatianaaaaaaa@users.noreply.github.com>
2022-12-08 21:42:52 +0200

    Update usercode.py

--- usercode.py
+++ usercode.py
@@ -1,12 +1,1 @@
-import jwt
-
-
-def handle(data): {
-    'sub': '4242',
-    'name': 'Jessica Temporal',
-    'nickname': 'Jess'
-}
-
-secret = 'my_super_secret'
-token = jwt.encode(payload=payload_data, key=secret)
-return data
+from cryptography.hazmat.primitives import serialization
