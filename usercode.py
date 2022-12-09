1ead9b034bca249bbd5d2076d65801e333318233
Tatianaaaaaaa <118683978+Tatianaaaaaaa@users.noreply.github.com>
2022-12-09 08:47:30 +0200

    Update usercode.py

--- usercode.py
+++ usercode.py
@@ -1,9 +1,5 @@
-import json
-from datetime import datetime, timedelta, timezone
+import jwt
 
-from jwt import (
-    JWT,
-    jwk_from_dict,
-    jwk_from_pem,
-)
-from jwt.utils import get_int_from_datetime
+def handle(data):
+    data["hello"] = "Hello world!"
+    return data
